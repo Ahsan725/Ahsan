80ef9bde3a19d544321940897fcfc173865ebbf5
Ahsan <sbaseer25@gmail.com>
2023-08-11 13:13:19 -0400

    Home page and navbar

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["a Full Stack Developer", "a Software Engineer", "skilled at Problem Solving"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
