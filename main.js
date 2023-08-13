d6f4841779165cf98c3d223dbbdaf5ad40ab8f89
Ahsan <sbaseer25@gmail.com>
2023-08-12 21:04:12 -0400

    changed the text bg

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var typed = new Typed(".text", {
-    strings: ["a Full Stack Developer", "a Software Engineer", "skilled at Problem Solving"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+  strings: [
+    "a Full Stack Developer.",
+    "a Software Engineer.",
+    "skilled at Problem Solving.",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 3000,
+  loop: true,
+});
