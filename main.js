1e84e2b953c6755abcaed92f1d226eadc2563f48
Ahsan <sbaseer25@gmail.com>
2023-08-13 03:30:00 -0400

    stable built 1.8

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ var typed = new Typed(".text", {
   backDelay: 3000,
   loop: true,
 });
+
+var typed1 = new Typed(".text1", {
+  strings: [
+    "Hello!",
+    "Let me briefly introduce myself."
+  ],
+  typeSpeed: 100,
+  backSpeed: 70,
+  backDelay: 3000,
+  loop: true,
+});
